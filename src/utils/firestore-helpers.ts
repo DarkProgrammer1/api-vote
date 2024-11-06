c1385c335c52b3d58588ab7e672515aa06b5f384
Nicolas Koumakoye <patritkoumakoyeloicnicolas@gmail.com>
2024-11-06 23:38:11 +0100

    Modifications dans firestore-helpers

--- src/utils/firestore-helpers.ts
+++ src/utils/firestore-helpers.ts
@@ -31,7 +31,7 @@ const dataPoint = <T>(collectionPath: string) => firestore.collection(collection
 const db: FirestoreCollections = { // Firestore collections
   users: dataPoint<User>('users'),
   posts: dataPoint<Post>('posts'),
-  comments: dataPoint<Post>('comments'),
+  comments: dataPoint<Comment>('comments'),
 };
 
 export { db, firestore, firestoreTimestamp };
